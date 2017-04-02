293bfef3bd933ef0da22dabd9cf705b4fa7e13fb
tomcat2088 <tomcat1991@126.com>
2017-04-02 22:40:04 +0800

    home

---
+++ app/components/stores/DaysStore.js
@@ -0,0 +1,3 @@
+/**
+ * Created by wangyang on 2017/4/2.
+ */
--- app/pages/HomePage.js
+++ app/pages/HomePage.js
@@ -12,7 +12,8 @@ import {
     Button,
     Image,
     TouchableOpacity,
-    ListView
+    ListView,
+    Animated
 } from 'react-native'
 
 var Overlay = require('react-native-overlay');
@@ -23,7 +24,9 @@ export default class HomePage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            eventMainAreaHeight: 160
+            eventMainAreaInitHeight: 200,
+            eventMainAreaHeight: 200,
+            eventMainAreaImageScale: 1
         }
     }
 
@@ -31,44 +34,96 @@ export default class HomePage extends Component {
         let ds = new ListView.DataSource({
             rowHasChanged: (r1, r2) => r1 !== r2
         });
+        let fontScale = this.state.eventMainAreaImageScale > 0.5 ? this.state.eventMainAreaImageScale : 0.5;
+        let opacity = 1 - 3*( 1 - this.state.eventMainAreaImageScale);
         return (
             <View style={{ flex: 1, backgroundColor: '#000000' }}>
-                <Image source={ require('../resources/bg_01.jpg')} style={{ position: 'absolute',  width:window.width, height:window.height}} >
-                    <BlurView blurType="dark" blurAmount={10} style={{ flex:1, width:window.width, height:window.height}}>
-                    </BlurView>
+                <Image source={ require('../resources/bg_01.jpg')} style={{ position: 'absolute', width:window.width, height:window.height}} >
                 </Image>
-                    <View style={{height: this.state.eventMainAreaHeight, overflow: 'hidden'}} >
-                        <Image
-                            style={{backgroundColor: '#ff0', width:window.width, height:window.height}}
-                            source={ require('../resources/bg_01.jpg') }
-                        >
-                            <View style={{ backgroundColor: '#000', flex: 1, alignItems: 'center',justifyContent: 'center', opacity: 0.4 }}>
-                            </View>
-                        </Image>
-                    </View>
-                    <View style={ styles.navigationBar }>
-                        <TouchableOpacity>
-                            <Text style={ styles.mainText }>菜单</Text>
-                        </TouchableOpacity>
-                        <Text style={ styles.mainText }>Our Days - Life</Text>
-                        <TouchableOpacity>
-                            <Text style={ styles.mainText }>添加</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <ListView
-                        style={ styles.eventList }
-                        dataSource={ ds.cloneWithRows([1, 2, 3, 5, 6]) }
-                        renderRow={ (data) => {
-                            return (
-                                <Text>{data}</Text>
-                            )
-                        } }
-                        onScroll={ (event)=> {
-                            let offsetY = event.nativeEvent.contentOffset.y;
-                            this.setState({ eventMainAreaHeight: offsetY < 0 ? 160 - offsetY : 160 });
+                <BlurView blurType="dark" blurAmount={10} style={{ position:'absolute', width:window.width, height:window.height}}>
+                </BlurView>
+                <Animated.View style={{
+                    backgroundColor: 'rgba(0,0,0, 0.0)',
+                    position: 'absolute',
+                    width: window.width,
+                    height: this.state.eventMainAreaHeight - 64,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    top: 64,
+                    overflow: 'hidden',
+                    opacity: opacity,
+                    transform: [{
+                        scaleX: fontScale
+                    }, {
+                        scaleY: fontScale
+                    }]
+                }}>
+                    <Text style={{
+                        fontSize: 44,
+                        color: '#fff'
+                    }}>3</Text>
+                    <Text style={{
+                        fontSize: 14,
+                        color: '#fff',
+                        marginTop: 3,
+                        marginBottom: 2
+                    }}>Event content</Text>
+                    <Text style={{
+                        fontSize: 10,
+                        color: '#fff'
+                    }}>2017-6-4(周五)</Text>
+                </Animated.View>
+                <View style={ styles.navigationBar } >
+                    <TouchableOpacity>
+                        <Text style={ styles.mainText }>菜单</Text>
+                    </TouchableOpacity>
+                    <Text style={ styles.mainText }>Our Days - Life</Text>
+                    <TouchableOpacity>
+                        <Text style={ styles.mainText }>添加</Text>
+                    </TouchableOpacity>
+                </View>
+                <ListView
+                    style={{
+                        overflow: 'visible',
+                        backgroundColor: '#00000000',
+                        marginTop: this.state.eventMainAreaInitHeight
+                    }}
+                    dataSource={ ds.cloneWithRows([1, 2, 3, 5, 6,7,8,9,3,4,5,6,7,8]) }
+                    renderRow={ (data) => {
+                        return (
+                        <View style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            height: 60,
+                            paddingLeft:16,
+                            paddingRight:16,
+                            paddingTop:10,
+                            paddingBottom:10,
+                            borderBottomColor: 'rgba(200,200,200,0.5)',
+                            borderBottomWidth: 1
                         }}>
+                            <View style={{ flexDirection: 'column'}}>
+                                <Text style={{color: '#fff', marginBottom: 5 }}>Event Name</Text>
+                                <Text style={{color: 'rgba(255,255,255,0.6)' }}>2017-8-9 (周日)</Text>
+                            </View>
+                            <Text style={{color: '#fff', fontSize: 28 }}>40</Text>
+                        </View>
 
-                    </ListView>
+                        )
+                    } }
+                    scrollEventThrottle = {16.0}
+                    onScroll={ (event)=> {
+                        let offsetY = event.nativeEvent.contentOffset.y;
+                        var scale = this.state.eventMainAreaHeight / this.state.eventMainAreaInitHeight;
+                        if (scale < 0) {
+                            scale = 0;
+                        }
+                        this.setState({ eventMainAreaHeight:  this.state.eventMainAreaInitHeight - offsetY });
+                        this.setState({ eventMainAreaImageScale: scale })
+                    }}>
+
+                </ListView>
 
             </View>
         )
@@ -95,8 +150,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#007',
     },
     eventList: {
-        overflow: 'visible',
-        backgroundColor: '#00000000',
+
     },
     mainText: {
         color: '#fff',
